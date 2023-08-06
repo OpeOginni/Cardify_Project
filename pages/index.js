46fde00950b8d6785e4f708b3e8de2d4c7f44448
OpeOginni <brightoginni123@gmail.com>
2023-08-06 09:33:29 +0100

    Cardify Update

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function HomePage() {
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
             <div className="sm:max-w-lg">
               <h1 className="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Banking is about YOU
+                Banking is about YOU!
               </h1>
               <p className="mt-4 text-xl text-gray-500">
                 This year, take your banking by storm. Order your finance cards
